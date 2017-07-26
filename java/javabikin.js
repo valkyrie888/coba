3e5dd14cbf35c04c81ccd12a67bcea163b9ad154
seanmantanhacker <pribadi.sean@gmail.com>
2017-07-26 12:50:31 +0700

    TES hide show

---
+++ java/javabikin.js
@@ -0,0 +1,17 @@
+
+function oyeng()
+{
+	document.getElementById("box1").style.display="block";
+}
+function oyeng1()
+{
+	document.getElementById("box1").style.display="none";
+}
+function oyeng2()
+{
+	document.getElementById("box2").style.display="block";
+}
+function oyeng3()
+{
+	document.getElementById("box2").style.display="none";
+}
